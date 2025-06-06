6a57884ec33b22c38d279743f88161f82f53e08e
KarolSalvador <karolsalvador91@gmail.com>
2025-06-06 09:31:23 -0300

    Envio do projeto

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const voltarAoTopo = document.getElementById('voltarAoTopo');
+
+window.addEventListener('scroll', function() {
+    console.log(window.scrollY);
+
+    if(window.scrollY >= 1500) {
+        voltarAoTopo.classList.add('mostrarBotao');
+
+    } else {
+        voltarAoTopo.classList.remove('mostrarBotao');
+    }
+});
+
+document.getElementById('enviarBtn').addEventListener('click', validaFormulario);
+
+const dadosUsuario = [];
+
+function validaFormulario(event) {
+    event.preventDefault();
+    if(document.getElementById('nome-do-usuario').value != "" && document.getElementById('email-do-usuario').value != "") {
+        alert("Prontinho! Obrigada pelo seu cadastro.");
+        
+        let nomeDigitado = document.getElementById('nome-do-usuario').value;
+        let emailDigitado = document.getElementById('email-do-usuario').value;
+
+        let dadosDoFormulario = {
+            nome: nomeDigitado,
+            email: emailDigitado
+        };
+
+        dadosUsuario.push(dadosDoFormulario);
+        console.log(dadosUsuario);
+
+    } else {
+        alert("Preencha os dados para envio do formulário.");
+    }
+
+}
